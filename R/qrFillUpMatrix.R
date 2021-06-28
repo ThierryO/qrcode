b70de01f3a91116d7cd4e1d6b43f86ccbdb321ab
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-06-28 14:17:27 +0200

    reduce cyclomatic complexity of qrFillUpMatrix()

--- R/qrFillUpMatrix.R
+++ R/qrFillUpMatrix.R
@@ -24,126 +24,27 @@ qrFillUpMatrix <- function(allBinary, data, version) {
     for (i in 1:8) {
       if (direction == 1) {
         #UP
-        if (
-          (pointer[2] %% 2 == 1 & pointer[2] > 7) |
-          (pointer[2] %% 2 == 0 & pointer[2] < 7)
-        ) {
+        if (is_even(pointer[2])) {
           #odd number, right-hand-side
-          if (data[pointer[1], pointer[2] - 1] == 0) {
-            pointer <- c(pointer[1], pointer[2] - 1)
-          } else if (data[pointer[1] - 1, pointer[2]] == 0) {
-            pointer <- c(pointer[1] - 1, pointer[2])
-          } else if (data[pointer[1], pointer[2] - 1] == 100) {
-            if (
-              data[pointer[1] - 1, pointer[2]] == 95 |
-              data[pointer[1] - 1, pointer[2]] == 55
-            ) {
-              pointer <- c(pointer[1] - 2, pointer[2])
-            }
-          } else {
-            stop("UP-ODD")
-          }
+          pointer <- up_odd(data, pointer)
         } else {
           #even number, left-hand-side
-          if (pointer[1] != 1) {
-            if (data[pointer[1] - 1, pointer[2] + 1] == 0) {
-              pointer <- c(pointer[1] - 1, pointer[2] + 1)
-            } else if (data[pointer[1] - 1, pointer[2]] == 0) {
-              pointer <- c(pointer[1] - 1, pointer[2])
-            } else if (data[pointer[1] - 1, pointer[2] + 1] == 20) {
-              if (
-                data[pointer[1], pointer[2] - 1] == 55 |
-                data[pointer[1], pointer[2] - 1] == 95
-              ) {
-                pointer <- c(pointer[1], pointer[2] - 2)
-              } else {
-                pointer <- c(pointer[1], pointer[2] - 1)
-              }
-              direction <- 2
-            } else if (data[pointer[1] - 1, pointer[2] + 1] == 100) {
-              if (data[pointer[1] - 1, pointer[2]] == 55) {
-                pointer <- c(pointer[1] - 2, pointer[2])
-              } else {
-                pointer <- c(pointer[1] - 6, pointer[2] + 1)
-              }
-            } else if (
-              data[pointer[1] - 1, pointer[2] + 1] == 95 |
-              data[pointer[1] - 1, pointer[2] + 1] == 55
-            ) {
-              #timing code
-              if (data[pointer[1] - 2, pointer[2] + 1] == 40) {
-                pointer <- c(1, pointer[2] - 2)
-                direction <- 2
-              } else {
-                pointer <- c(pointer[1] - 2, pointer[2] + 1)
-              }
-            } else {
-              stop("UP-EVEN")
-            }
-          } else{
-            pointer <- c(pointer[1], pointer[2] - 1)
-            direction <- 2
-          }
+          pointer <- up_even(data, pointer)
+          direction <- ifelse(
+            is.null(attr(pointer, "direction")), 1, attr(pointer, "direction")
+          )
         }
       } else {
         #DOWN
-        if (
-          (pointer[2] %% 2 == 1 & pointer[2] > 7) |
-          (pointer[2] %% 2 == 0 & pointer[2] < 7)
-        ) {
+        if (is_even(pointer[2])) {
           #odd number, right-hand-side
-          if (data[pointer[1], pointer[2] - 1] == 0) {
-            pointer <- c(pointer[1], pointer[2] - 1)
-          } else if (data[pointer[1] + 1, pointer[2]] == 0) {
-            pointer <- c(pointer[1] + 1, pointer[2])
-          } else if (data[pointer[1], pointer[2] - 1] == 100) {
-            if (
-              data[pointer[1] + 1, pointer[2]] == 95 |
-              data[pointer[1] + 1, pointer[2]] == 55
-            ) {
-              pointer <- c(pointer[1] + 2, pointer[2])
-            } else {
-              pointer <- c(pointer[1] + 1, pointer[2])
-            }
-          } else {
-            stop("DOWN-ODD")
-          }
+          pointer <- down_odd(data, pointer)
         } else {
           #even number, left-hand-side
-          if (pointer[1] != size) {
-            if (data[pointer[1] + 1, pointer[2] + 1] == 0) {
-              pointer <- c(pointer[1] + 1, pointer[2] + 1)
-            } else if (data[pointer[1] + 1, pointer[2]] == 0) {
-              pointer <- c(pointer[1] + 1, pointer[2])
-            } else if (data[pointer[1] + 1, pointer[2] + 1] == 100) {
-              if (data[pointer[1] + 1, pointer[2]] == 55) {
-                pointer <- c(pointer[1] + 2, pointer[2])
-              } else {
-                pointer <- c(pointer[1] + 6, pointer[2] + 1)
-              }
-            } else if (
-              data[pointer[1] + 1, pointer[2] + 1] == 50 |
-              data[pointer[1] + 1, pointer[2] + 1] == 40
-            ) {
-              pointer <- c(pointer[1], pointer[2] - 1)
-              direction <- 1
-            } else if (
-              data[pointer[1] + 1, pointer[2] + 1] == 95 |
-              data[pointer[1] + 1, pointer[2] + 1] == 55
-            ) {
-              pointer <- c(pointer[1] + 2, pointer[2] + 1)
-            } else {
-              stop("DOWN-EVEN")
-            }
-          } else {
-            if (data[pointer[1], pointer[2] - 1] == 20) {
-              #left botom eye
-              pointer <- c(pointer[1] - 8, pointer[2] - 1)
-            } else {
-              pointer <- c(pointer[1], pointer[2] - 1)
-            }
-            direction <- 1
-          }
+          pointer <- down_even(data, pointer, size)
+          direction <- ifelse(
+            is.null(attr(pointer, "direction")), 2, attr(pointer, "direction")
+          )
         }
       }
       if (i < 8) {
@@ -153,4 +54,119 @@ qrFillUpMatrix <- function(allBinary, data, version) {
     }
   }
   return(data)
+}
+
+is_even <- function(x) {
+  (x %% 2 == 1 && x > 7) || (x %% 2 == 0 && x < 7)
+}
+
+up_odd <- function(data, pointer) {
+  if (data[pointer[1], pointer[2] - 1] == 0) {
+    return(c(pointer[1], pointer[2] - 1))
+  }
+  if (data[pointer[1] - 1, pointer[2]] == 0) {
+    return(c(pointer[1] - 1, pointer[2]))
+  }
+  if (data[pointer[1], pointer[2] - 1] == 100) {
+    return(
+      c(
+        pointer[1] +
+          ifelse(data[pointer[1] - 1, pointer[2]] %in% c(55, 95), -2, 0),
+        pointer[2]
+      )
+    )
+  }
+  stop("UP-ODD")
+}
+
+down_odd <- function(data, pointer) {
+  if (data[pointer[1], pointer[2] - 1] == 0) {
+    return(c(pointer[1], pointer[2] - 1))
+  }
+  if (data[pointer[1] + 1, pointer[2]] == 0) {
+    return(c(pointer[1] + 1, pointer[2]))
+  }
+  if (data[pointer[1], pointer[2] - 1] == 100) {
+    return(
+      c(
+        pointer[1] +
+          ifelse(data[pointer[1] + 1, pointer[2]] %in% c(55, 95), 2, 1),
+        pointer[2]
+      )
+    )
+  }
+  stop("DOWN-ODD")
+}
+
+up_even <- function(data, pointer) {
+  if (pointer[1] == 1) {
+    output <- c(pointer[1], pointer[2] - 1)
+    attr(output, "direction") <- 2
+    return(output)
+  }
+  if (data[pointer[1] - 1, pointer[2] + 1] == 0) {
+    return(c(pointer[1] - 1, pointer[2] + 1))
+  }
+  if (data[pointer[1] - 1, pointer[2]] == 0) {
+    return(c(pointer[1] - 1, pointer[2]))
+  }
+  if (data[pointer[1] - 1, pointer[2] + 1] == 20) {
+    output <- c(
+      pointer[1],
+      pointer[2] - ifelse(data[pointer[1], pointer[2] - 1] %in% c(55, 95), 2, 1)
+    )
+    attr(output, "direction") <- 2
+    return(output)
+  }
+  if (data[pointer[1] - 1, pointer[2] + 1] == 100) {
+    return(
+      c(
+        pointer[1] - ifelse(data[pointer[1] - 1, pointer[2]] == 55, 2, 6),
+        pointer[2] + ifelse(data[pointer[1] - 1, pointer[2]] == 55, 0, 1)
+      )
+    )
+  }
+  if (data[pointer[1] - 1, pointer[2] + 1] %in% c(55, 95)) {
+    if (data[pointer[1] - 2, pointer[2] + 1] == 40) {
+      output <- c(1, pointer[2] - 2)
+      attr(output, "direction") <- 2
+      return(output)
+    }
+    return(c(pointer[1] - 2, pointer[2] + 1))
+  }
+  stop("UP-EVEN")
+}
+
+down_even <- function(data, pointer, size) {
+  if (pointer[1] == size) {
+    output <- c(
+      pointer[1] - ifelse(data[pointer[1], pointer[2] - 1] == 20, 8, 0),
+      pointer[2] - 1
+    )
+    attr(output, "direction") <- 1
+    return(output)
+  }
+  if (data[pointer[1] + 1, pointer[2] + 1] == 0) {
+    return(c(pointer[1] + 1, pointer[2] + 1))
+  }
+  if (data[pointer[1] + 1, pointer[2]] == 0) {
+    return(c(pointer[1] + 1, pointer[2]))
+  }
+  if (data[pointer[1] + 1, pointer[2] + 1] == 100) {
+    return(
+      c(
+        pointer[1] + ifelse(data[pointer[1] + 1, pointer[2]] == 55, 2, 6),
+        pointer[2] + ifelse(data[pointer[1] + 1, pointer[2]] == 55, 0, 1)
+      )
+    )
+  }
+  if (data[pointer[1] + 1, pointer[2] + 1] %in% c(40, 50)) {
+    output <- c(pointer[1], pointer[2] - 1)
+    attr(output, "direction") <- 1
+    return(output)
+  }
+  if (data[pointer[1] + 1, pointer[2] + 1] %in% c(55, 95)) {
+    return(c(pointer[1] + 2, pointer[2] + 1))
+  }
+  stop("DOWN-EVEN")
 }
