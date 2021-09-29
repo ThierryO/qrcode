d638432c4f6f474ffb5965113a9f7ed250625416
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-09-29 17:09:01 +0200

    add qr_mode()

---
+++ R/qr_mode.R
@@ -0,0 +1,29 @@
+#' Determine the required mode
+#' @param x the input string
+#' @return a character indicating the mode
+#' @export
+#' @importFrom assertthat assert_that is.string noNA
+qr_mode <- function(x) {
+  assert_that(is.string(x), noNA(x))
+  if (grepl("^[0-9]*$", x)) {
+    return("Numeric")
+  }
+  regexp <- paste(c("^[", names(alphanum()), "]*$"), collapse = "")
+  if (grepl(regexp, x)) {
+    return("Alphanumeric")
+  }
+  if (!Encoding(x) %in% c("unknown", "latin1")) {
+    x <- iconv(x, Encoding(x), "latin1")
+  }
+  assert_that(
+    Encoding(x) %in% c("unknown", "latin1"), noNA(x),
+    msg = "current version handles only latin1 characters"
+  )
+  return("Byte")
+}
+
+alphanum <- function() {
+  structure(
+    0:44, .Names = c(0:9, LETTERS, " ", "$", "%", "*", "+", "-", ".", "/", ":")
+  )
+}
---
+++ tests/testthat/test_a_qr_mode.R
@@ -0,0 +1,11 @@
+test_that("qr_mode() works as expected", {
+  expect_identical(qr_mode("0"), "Numeric")
+  expect_identical(qr_mode("0.1"), "Alphanumeric")
+  expect_identical(qr_mode("A"), "Alphanumeric")
+  expect_identical(qr_mode("a"), "Byte")
+  x <- "fa\xE7ile"
+  Encoding(x) <- "latin1"
+  expect_identical(qr_mode(x), "Byte")
+  expect_identical(qr_mode("\u00E6"), "Byte")
+  expect_error(qr_mode("\u2192"), "only latin1")
+})
