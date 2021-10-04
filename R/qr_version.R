6d5112c29e4dee2be5b52327d9f62e47476c48ef
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-10-04 15:23:40 +0200

    add qr_encode()

--- R/qr_version.R
+++ R/qr_version.R
@@ -7,10 +7,10 @@ qr_version <- function(x, ecl = c("L", "M", "Q", "H")) {
   ecl <- match.arg(ecl)
   relevant <- qrCodeSpec[qrCodeSpec$ECL == ecl, ]
   qm <- qr_mode(x)
-  byte_string <- list(
-    Numeric = c(FALSE, FALSE, FALSE, TRUE),
-    Alphanumeric = c(FALSE, FALSE, TRUE, FALSE),
-    Byte = c(FALSE, TRUE, FALSE, FALSE)
+  bit_string <- list(
+    Numeric = bits(c(FALSE, FALSE, FALSE, TRUE)),
+    Alphanumeric = bits(c(FALSE, FALSE, TRUE, FALSE)),
+    Byte = bits(c(FALSE, TRUE, FALSE, FALSE))
   )[[qm]]
   relevant <- relevant[head(which(nchar(x) <= relevant[[qm]]), 1), ]
   indicator <- matrix(
@@ -18,13 +18,49 @@ qr_version <- function(x, ecl = c("L", "M", "Q", "H")) {
     dimnames = list(NULL, c("Numeric", "Alphanumeric", "Byte"))
   )
   count_length <- indicator[cut(relevant$Version, c(0, 9, 26, 40)), qm]
-  count <- as.logical(rev(rawToBits(as.raw(nchar(x)))))
-  count <- tail(count, max(which(rev(count))))
-  byte_string <- c(byte_string, rep(FALSE, count_length - length(count)), count)
+  count <- bits(as.logical(rev(rawToBits(as.raw(nchar(x))))))
+  assert_that(
+    length(count) <= count_length,
+    msg = paste(
+      "count longer than expected in qr_version().",
+      "Please contact the maintainer and",
+      "provide the code that triggered this error."
+    )
+  )
+  bit_string <- c(bit_string, rep(FALSE, count_length - length(count)), count)
+  remainder <- ifelse(
+    relevant$Version < 14,
+    ifelse(relevant$Version < 7, ifelse(relevant$Version < 2, 0, 7), 0),
+    ifelse(
+      relevant$Version < 28,
+      ifelse(relevant$Version < 21, 3, 4),
+      ifelse(relevant$Version < 35, 3, 0)
+    )
+  )
+  list(
+    NULL, c(6L, 18L), c(6L, 22L), c(6L, 26L), c(6L, 30L), c(6L, 34L),
+    c(6L, 22, 38L), c(6L, 22L, 42L), c(6L, 26L, 46L), c(6L, 28L, 50L),
+    c(6L, 30L, 54L), c(6L, 32L, 58L), c(6L, 34L, 62L), c(6L, 26L, 46L, 66L),
+    c(6L, 26L, 48L, 70L), c(6L, 26L, 50L, 74L), c(6L, 26L, 50L, 74L),
+    c(6L, 26L, 54L, 78L), c(6L, 26L, 56L, 82L), c(6L, 26L, 58L, 86L),
+    c(6L, 34L, 62L, 90L), c(6L, 28L, 50L, 72L, 94L), c(6L, 26L, 50L, 74L, 98L),
+    c(6L, 30L, 54L, 78L, 102L), c(6L, 28L, 54L, 80L, 106L),
+    c(6L, 32L, 58L, 84L, 110L), c(6L, 30L, 58L, 86L, 114L),
+    c(6L, 34L, 62L, 90L, 118L), c(6L, 26L, 50, 74L, 98L, 122L),
+    c(6L, 30L, 54L, 78L, 102L, 126L), c(6L, 26L, 52L, 78L, 104L, 130L),
+    c(6L, 30L, 56L, 82L, 108L, 134L), c(6L, 34L, )
+  )
+  attr(bit_string, "version") <- relevant$Version
+  attr(bit_string, "ecl") <- ecl
+  attr(bit_string, "dcword1") <- relevant$DCinGrp1
+  attr(bit_string, "n1") <- relevant$Grp1
+  attr(bit_string, "dcword2") <- relevant$DCinGrp2
+  attr(bit_string, "n2") <- relevant$Grp2
+  attr(bit_string, "ecword") <- relevant$ECwordPerBlock
+  attr(bit_string, "remainder") <- remainder
   return(
     list(
-      version = relevant$Version, ecl = ecl, mode = qm,
-      byte_string = byte_string
+      version = relevant$Version, ecl = ecl, mode = qm, bit_string = bit_string
     )
   )
 }
