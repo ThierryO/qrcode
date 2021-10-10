5a3c2cf5a95325885df3ed642fcb1c48a5442b2c
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-10-10 22:49:57 +0200

    bugfix in qr codes with version >= 7

--- tests/testthat/test_d_qr_code.R
+++ tests/testthat/test_d_qr_code.R
@@ -1,27 +1,28 @@
 test_that("qr_code() has a stable output for numeric", {
+  skip_if_not(Sys.getenv("DETAILED") != "", "Skipping detailed checks.")
   skip_on_cran()
+  skip_on_covr()
   test_string <- c(
     "0", "1", "12", "123", "1234", "12345", "123456", "1230", "12300", "123000"
   )
   for (i in seq_len(length(test_string) * 4) - 1) {
     x <- test_string[i %/% 4 + 1]
     ecl <- c("L", "M", "Q", "H")[i %% 4 + 1]
-    expect_snapshot_value(
-      qr_code(x = x, ecl = ecl), cran = TRUE, style = "json2"
-    )
+    expect_true(validate_qr(x = x, ecl = ecl))
   }
 })
 
 test_that("qr_code() has a stable output for alphanumeric", {
+  skip_if_not(Sys.getenv("DETAILED") != "", "Skipping detailed checks.")
   skip_on_cran()
+  skip_on_covr()
+  test_string <- c(LETTERS, 0:9, " ", "$", "%", "*", "+", "-", ".", "/", ":")
   test_string <- rep(
-    LETTERS, ceiling(max(qrCodeSpec$Alphanumeric) / length(LETTERS))
+    test_string, ceiling(max(qrCodeSpec$Alphanumeric) / length(test_string))
   )
-  for (i in seq_len(6 * 4)) {
+  for (i in seq_along(qrCodeSpec$Alphanumeric)) {
     x <- paste(head(test_string, qrCodeSpec$Alphanumeric[i]), collapse = "")
     ecl <- as.character(qrCodeSpec$ECL[i])
-    expect_snapshot_value(
-      qr_code(x = x, ecl = ecl), cran = TRUE, style = "json2"
-    )
+    expect_true(validate_qr(x = x, ecl = ecl))
   }
 })
