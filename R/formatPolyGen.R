b137dd436f00e3c13c2877a9df9b699db5675473
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-11-15 21:55:17 +0100

    defunct legacy functions

--- R/formatPolyGen.R
+++ R/formatPolyGen.R
@@ -1,32 +1,7 @@
-#' Function to calculate and generate format polynomial
-#'
-#' @param formatString QRcode format binary string
-#' @param polyString polynomial to create ECL for formatString
 #' @author Victor Teh
 #' @family legacy
-#'
-#'
+#' @rdname defunct
 
-formatPolyGen <- function(formatString, polyString) {
-  .Deprecated("qr_code")
-
-  formatString <- as.integer(unlist(strsplit(formatString, split = "")))
-  oriFormatString <- formatString
-  polyString <- as.integer(unlist(strsplit(polyString, split = "")))
-  formatString <- c(formatString, rep(0, 10))
-
-  for (i in 1:5) {
-    if (formatString[1] == 0) {
-      formatString <- formatString[2:length(formatString)]
-    } else {
-      polyTemp <- c(
-        polyString, rep(0, length(formatString) - length(polyString))
-      )
-      formatString <- bitwXor(formatString, polyTemp)
-      formatString <- formatString[2:length(formatString)]
-    }
-  }
-  formatString <- c(oriFormatString, formatString)
-  finalPoly <- as.integer(unlist(strsplit("101010000010010", split = "")))
-  return(bitwXor(formatString, finalPoly))
+formatPolyGen <- function(...) { # nolint: object_name_linter.
+  .Defunct("qr_code")
 }
