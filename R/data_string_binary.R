9b16f1e9de966be237770775a9715b9abe8b3c9f
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-11-22 14:15:57 +0100

    remove unexported defunct functions

--- R/data_string_binary.R
+++ R/data_string_binary.R
@@ -2,6 +2,7 @@
 #' @author Victor Teh
 #' @family legacy
 #' @rdname defunct
+#' @param ... obsolete arguments
 DataStringBinary <- function(...) { #nolint: object_name_linter.
   .Defunct("qr_code")
 }
--- R/qr_init_matrix.R
+++
@@ -1,7 +0,0 @@
-#' @author Victor Teh
-#' @family legacy
-#' @rdname defunct
-
-qrInitMatrix <- function(...) { # nolint: object_name_linter.
-  .Defunct("qr_code")
-}
