a3f0dad882953af0ec4f7c7cccc2133d2436916b
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-12-04 13:26:57 +0100

    qr_logo() return a qr_logo object instead of writing an svg file

--- R/qr_logo.R
+++ R/qr_logo.R
@@ -2,38 +2,54 @@
 #'
 #' Will create an `svg` file with the QR code and logo.
 #' @param logo the path to a logo image file
-#' @inheritParams qr_code
-#' @inheritParams generate_svg
-#' @importFrom assertthat assert_that is.string
-#' @importFrom knitr image_uri
+#' @param ecl the required error correction level for the remaining QR code
+#' covered by the logo.
+#' Available options are `"L"` (7%), `"M"` (15%), `"Q"` (25%) and `"H"` (30%).
+#' Defaults to `"L"`.
+#' @param input_ecl the required error correction level for the QR code
+#' underneath the logo.
+#' Must be higher than the selected level in `ecl`.
+#' Defaults to `"H"`.
+#' The difference between `ecl` and `input_ecl` determines the size of the logo.
 #' @export
 qr_logo <- function(
-  x, logo, filename, size = 300, foreground = "black", background = "white",
-  show = interactive()
+  x, logo, ecl = c("L", "M", "Q", "H"), input_ecl = c("H", "Q", "M", "L")
 ) {
-  assert_that(is.string(x))
-  assert_that(file.exists(logo))
-  ecl <- "L"
-  input_ecl <- c(L = "H")[ecl]
-  qr_code(x = x, ecl = input_ecl) |>
-    generate_svg(
-      filename = filename, size = size, foreground = foreground,
-      background = background, show = TRUE
-    )
-  svg <- readLines(filename)
-  n_svg <- length(svg)
+  ecl <- match.arg(ecl)
+  input_ecl <- match.arg(input_ecl)
   error_level <- c(L = 0.07, M = 0.15, Q = 0.25, H = 0.3)
-  logo_width <- (error_level[input_ecl] - error_level[ecl]) * size
-  img <- paste(
-    "  <image href = \"%1$s\" x = \"%2$.0f\" y = \"%2$.0f\"",
-    "width = \"%3$.0f\" height = \"%3$.0f\" />"
-  ) |>
-    sprintf(image_uri(logo), (size - logo_width) / 2, logo_width)
-  svg[-n_svg] |>
-    c(img, svg[n_svg]) |>
-    writeLines(filename)
-  if (show) {
-    browseURL(filename)
-  }
-  return(filename)
+  error_in <- error_level[input_ecl]
+  error_out <- error_level[ecl]
+  assert_that(
+    error_in > error_out,
+    msg = "error level in `input_ecl` must be higher than in `ecl`"
+  )
+  logo <- read_logo(logo = logo)
+  logo_ratio <- attr(logo, "height") / attr(logo, "width")
+  er <- (error_out -  error_in) / (error_out - 1)
+  code <- qr_code(x = x, ecl = input_ecl)
+  attr(code, "logo") <- logo
+  info_size <- prod(dim(code)) # total size
+  info_size <- info_size - 3 * 8 ^ 2 # finder patterns
+  info_size <- info_size - 2 * (ncol(code) - 2 * 8) # timing patterns
+  info_size <- info_size - 2 * 3 * 6 + 1 # information area
+  sqrt(er * info_size / logo_ratio) |>
+    unname() -> attr(code, "logo_width")
+  attr(code, "logo_height") <- attr(code, "logo_width") * logo_ratio
+  class(code) <- c("qr_logo", class(code))
+  return(code)
+}
+
+
+#' @importFrom assertthat assert_that is.string noNA
+read_logo <- function(logo) {
+  assert_that(is.string(logo), noNA(logo), file.exists(logo))
+  extension <- gsub(".*\\.(.*)?", "\\1", logo)
+  assert_that(extension %in% c("png"), msg = "Currently only handles png logos")
+  requireNamespace("png")
+  mat <- png::readPNG(logo)
+  attr(mat, "type") <- "raster"
+  attr(mat, "width") <- dim(mat)[1]
+  attr(mat, "height") <- dim(mat)[2]
+  return(mat)
 }
