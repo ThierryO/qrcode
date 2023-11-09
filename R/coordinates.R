cc32e10d1081c99468fcc5bfcabd1762c36b2874
Thierry Onkelinx <qrcode@muscardinus.be>
2023-11-09 21:13:33 +0100

    improve documentation

--- R/coordinates.R
+++ R/coordinates.R
@@ -1,12 +1,16 @@
-#' Extract coordinates from a QR code object
+#' Extract coordinates from a QR code object.
+#'
 #' Selects the dark elements from the `qr_code` object and returns their
 #' coordinates.
+#' This can be useful when you want to create a QR code with a custom style.
 #' @param x the `qr_code` object.
 #' @returns A `data.frame` with the `column` and `row` number of the dark
 #' elements.
 #' @examples
 #' x <- qr_code("test")
 #' plot(x)
+#' head(coordinates(x))
+#' plot(coordinates(x), pch = 19, cex = 2, asp = 1)
 #' @author Thierry Onkelinx
 #' @family qr
 #' @export
